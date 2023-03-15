15d3633176ca1d9daa05d5e1c86b9bf6e1d4a1a2
Automation is everything <38140709+PuPpY-ThEAuToMaTiOnIsT@users.noreply.github.com>
2023-03-15 16:43:32 +0530

    Update Script_1_to_get_list_of_services.ps1
    Added change to script to make sure the data of all the existing services is created on Desktop

--- Script_1_to_get_list_of_services.ps1
+++ Script_1_to_get_list_of_services.ps1
@@ -1,7 +1,8 @@
 #Run the below in Windows PowerShell (Admin) [ Right click on start menu icon and open this ]
-#An excel will be created in the location from where the script is located
+#An excel will be created in your desktop
 #Now get the service name list which you want to disable and change it into comma separated values with service names in double quotes
 
+Add-Type -AssemblyName System.Windows.Forms
 $services = Get-Service | Select-Object -Property Name, DisplayName, Description, Status, StartType
 $excel = New-Object -ComObject Excel.Application
 $workbook = $excel.Workbooks.Add()
@@ -21,7 +22,9 @@ foreach ($service in $services) {
     $worksheet.Cells.Item($row,5) = $service.StartType
     $row++
 }
-$filename = [System.IO.Path]::Combine((Get-Location).Path, "services.xlsx")
+$filename = [System.IO.Path]::Combine($desktopPath, "services_list.xlsx")
+$excel.DisplayAlerts = $false
 $workbook.SaveAs($filename)
 $workbook.Close()
 $excel.Quit()
+[System.Windows.Forms.MessageBox]::Show("The file has been generated and saved to your desktop.", "File Generated", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
